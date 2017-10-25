359fdee0295f1f734564a12a26e12ed33e5bda0c
ehsan <ehsan@ebi.ac.uk>
2017-10-25 16:16:06 +0100

    Solving Latest studies bug in the index.js

--- src/main/webapp/js/index.js
+++ src/main/webapp/js/index.js
@@ -5,7 +5,7 @@
     // Prepare template
     var template = Handlebars.compile($('script#stats-template').html());
 
-    $.getJSON( contextPath + "/api/v1/search",{query:'type:Study',pageSize:5,sortby:'release_date'}, function( data ) {
+    $.getJSON( contextPath + "/api/v1/search",{query:'type:Study',pageSize:5,sortBy:'release_date',sortOrder:'descending'}, function( data ) {
          if (data) {
              var totalCount = data.totalHits + (data.totalHits == 1 ? ' study' : ' studies');
              $('#template').html(template(data));
