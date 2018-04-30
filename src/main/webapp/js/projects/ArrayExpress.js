8d69dfc28694d4220d28cf3aa4648a6b29e08fa0
Awais Athar <awais@windows.ebi.ac.uk>
2018-04-30 18:17:18 +0100

    Made No. of samples clickable [Delivers #157198134]

--- src/main/webapp/js/projects/ArrayExpress.js
+++ src/main/webapp/js/projects/ArrayExpress.js
@@ -1,8 +1,21 @@
 $(sectionTables).each(function () {
     var sampleColumn =  this.columns(function(i,d,n){ return $(n).text()=='No. of Samples' });
     if (!sampleColumn || !sampleColumn.length ||!sampleColumn.nodes()[0] ||  !sampleColumn.nodes()[0].length) return;
-    /*$(sampleColumn.nodes()[0]).each( function() {
-        $(this).wrapInner('<a href="#"></a>'
-    )})*/
+    $(sampleColumn.nodes()[0]).each( function(i,v) {
+        var id = generatedID++;
+        $(v).wrapInner('<a id="genid' +  id  + '" name="genid'+ id +'" href="#"></a>');
+        $('a',v).attr('data-file-data-search',$(v).siblings().map(function(){ return $(this).text(); }).toArray().join(' '));
+    })
 
 });
+
+$('a[data-file-data-search]').click( function () {
+    expansionSource = ''+$(this).attr('id');
+    clearFileFilter();
+    $('#all-files-expander').click();
+    filesTable.search($(this).data('file-data-search'));
+    // hide empty columns
+    filesTable.columns().every(function(){ if (filesTable.cells({search:'applied'},this).data().join('').trim()=='') this.visible(false) });
+    filesTable.draw();
+    return false;
+})
