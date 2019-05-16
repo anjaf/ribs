f69842d5dbdf9d669d3cc3665889aa08bfe02527
Awais Athar <awais@ebi.ac.uk>
2019-05-16 14:14:28 +0100

    Fixed imports

--- src/main/java/uk/ac/ebi/biostudies/api/util/parser/EUToxRiskDataTypeParser.java
+++ src/main/java/uk/ac/ebi/biostudies/api/util/parser/EUToxRiskDataTypeParser.java
@@ -2,18 +2,11 @@ package uk.ac.ebi.biostudies.api.util.parser;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import com.jayway.jsonpath.ReadContext;
-import net.minidev.json.JSONArray;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.apache.xpath.operations.Bool;
 import uk.ac.ebi.biostudies.api.util.Constants;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Map;
-import java.util.stream.Collectors;
-
-import static uk.ac.ebi.biostudies.api.util.Constants.NA;
 
 public class EUToxRiskDataTypeParser extends AbstractParser{
     private static final Logger LOGGER = LogManager.getLogger(EUToxRiskDataTypeParser.class.getName());
