de2aa9339d34cd383c10faa212322a75703727b5
Awais Athar <awais@ebi.ac.uk>
2019-08-09 15:07:25 +0100

    Fixed auth cookie bug Refactored SpringBootLoader class Added a dummy default-project file

--- src/main/java/uk/ac/ebi/biostudies/BioStudiesUI.java
+++ src/main/java/uk/ac/ebi/biostudies/BioStudiesUI.java
@@ -5,17 +5,16 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootApplication
 //@EnableWebMvc
-public class SpringBootLoader extends SpringBootServletInitializer {
+public class BioStudiesUI extends SpringBootServletInitializer {
     @Override
     protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-        return application.sources(SpringBootLoader.class);
+        return application.sources(BioStudiesUI.class);
     }
 
     public static void main(String[] args) {
-        SpringApplication.run(SpringBootLoader.class, args);
+        SpringApplication.run(BioStudiesUI.class, args);
     }
 }
--- src/main/java/uk/ac/ebi/biostudies/auth/CookieFilter.java
+++ src/main/java/uk/ac/ebi/biostudies/auth/CookieFilter.java
@@ -3,6 +3,7 @@ package uk.ac.ebi.biostudies.auth;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 import org.springframework.web.context.support.SpringBeanAutowiringSupport;
 import uk.ac.ebi.biostudies.api.util.CookieMap;
 import uk.ac.ebi.biostudies.api.util.HttpTools;
@@ -14,6 +15,7 @@ import java.io.IOException;
 /**
  * Created by ehsan on 15/03/2017.
  */
+@Component
 public class CookieFilter implements Filter {
 
     private Logger logger = LogManager.getLogger(CookieFilter.class.getName());
