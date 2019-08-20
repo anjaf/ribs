4a130f69a66f25f902c22feac173f793fdd51e6c
behrangi <ehsan.behrangi@gmail.com>
2019-08-20 15:31:20 +0100

    making environment aware scripts 1

--- tasks/makewar.sh
+++ tasks/makewar.sh
@@ -19,16 +19,15 @@ eval sshKey='$'"$1_key"
 eval submissionsFilePath='$'"submissions_file_path_$1"
 eval ftpUrl='$'"ftp_url_$1"
 
-
-sed -i -e "s/@studies.json@/$studiesJsonAddress/g" ./src/main/resources/index.properties
-sed -i -e "s/@subfilepath@/$submissionsFilePath/g" ./src/main/resources/index.properties
-sed -i -e "s/@ftpurl@/$ftpUrl/g" ./src/main/resources/index.properties
+sed -i -v -e "s@studies.json@$studiesJsonAddress@g" ./src/main/resources/index.properties
+sed -i -v -e "s/subfilepath/${submissionsFilePath}/g" ./src/main/resources/index.properties
+sed -i -e "s/ftpurl/${ftpUrl}/g" ./src/main/resources/index.properties
 
 
 mkdir -p ~/.ssh
 echo "$sshKey" | tr -d '\r' > ~/.ssh/id_rsa
 chmod 700 ~/.ssh/id_rsa
- eval $(ssh-agent -s)
+eval $(ssh-agent -s)
 ssh-add ~/.ssh/id_rsa
 echo 'copy to ribs'
 mvn clean install spring-boot:repackage
