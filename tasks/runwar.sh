9ce43adacac2f49a317287eb9863d2562f04a784
behrangi <ehsan.behrangi@gmail.com>
2019-08-28 16:27:24 +0100

    making environment aware scripts 1

--- tasks/runwar.sh
+++ tasks/runwar.sh
@@ -13,8 +13,8 @@ echo "$sshKey" | tr -d '\r' > ~/.ssh/id_rsa
 chmod 700 ~/.ssh/id_rsa
 eval $(ssh-agent -s)
 ssh-add ~/.ssh/id_rsa
-ssh -o StrictHostKeyChecking=no "$4@$3" "cd $2; bash" <<'ENDSSH'
+ssh -o StrictHostKeyChecking=no "$4@$3 jdk11=$5" "cd $2; bash" <<'ENDSSH'
 pkill -9 -f war
-/nfs/ma/home/java/jdk-11.0.2/bin/java -Dtomcat.hostname=$(hostname -s) -Xmx12G -jar ./biostudies.war > /dev/null 2>&1 &
+$jdk11 -Dtomcat.hostname=$(hostname -s) -Xmx12G -jar ./biostudies.war > /dev/null 2>&1 &
 disown -h %1
 ENDSSH
