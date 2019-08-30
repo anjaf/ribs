f2cb58b8fb63a8996dd2c12c1b9d93af76d79fcd
behrangi <ehsan.behrangi@gmail.com>
2019-08-30 12:34:34 +0100

    making environment aware scripts 1

--- tasks/runwar.sh
+++ tasks/runwar.sh
@@ -7,11 +7,12 @@ echo $1
 echo $2
 
 eval sshKey='$'"$1_key"
-sed -i -e "s@jdkhome@${5}@g" ./runscript.sh
+
+sed -i -e "s@jdkhome@${5}@g" ./tasks/runscript.sh
 
 mkdir -p ~/.ssh
 echo "$sshKey" | tr -d '\r' > ~/.ssh/id_rsa
 chmod 700 ~/.ssh/id_rsa
 eval $(ssh-agent -s)
 ssh-add ~/.ssh/id_rsa
-ssh -o StrictHostKeyChecking=no "$4@$3" "cd $2; bash -s" < ./runscript.sh
+ssh -o StrictHostKeyChecking=no "$4@$3" "cd $2; bash -s" < ./tasks/runscript.sh
