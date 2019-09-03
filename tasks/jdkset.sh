96e54babf89da9cbbd1360715621cefa338efb77
behrangi <ehsan.behrangi@gmail.com>
2019-09-03 11:05:29 +0100

    making environment aware scripts 1

--- tasks/jdkset.sh
+++ tasks/jdkset.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
 sed -i -e "s@jdkhome@${1}@g" ./tasks/runwar.sh
+sed -i -e "s@waraddress@${2}@g" ./tasks/runwar.sh
--- tasks/runwar.sh
+++ tasks/runwar.sh
@@ -13,7 +13,8 @@ echo "$sshKey" | tr -d '\r' > ~/.ssh/id_rsa
 chmod 700 ~/.ssh/id_rsa
 eval $(ssh-agent -s)
 ssh-add ~/.ssh/id_rsa
-ssh -o StrictHostKeyChecking=no "$4@$3" "cd $2; " <<'ENDSSH'
+ssh -oStrictHostKeyChecking=no "$3@$2" <<'ENDSSH'
+cd waraddress
 pkill -9 -f biostudies
 jdkhome -Dbiostudies -Dtomcat.hostname=$(hostname -s) -Xmx12G -jar ./biostudies.war > /dev/null 2>&1 &
 ENDSSH
