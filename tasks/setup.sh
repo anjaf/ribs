a5248388fc057aed9e74bd8f39089095206adecd
Awais Athar <awais@ebi.ac.uk>
2019-10-02 14:48:26 +0100

    Removed redundant code from setup

--- tasks/setup.sh
+++ tasks/setup.sh
@@ -1,18 +1,12 @@
 #!/bin/bash
-set -e
-set -v
-
-eval jdkHome='$'"jdkHome"
-eval deployDirectory='$'"deployDirectory"
-sed -i -e "s@jdkhome@${jdkHome}@g" ./tasks/deploy.sh
-sed -i -e "s@waraddress@${deployDirectory}@g" ./tasks/deploy.sh
-
 echo "Updating properties files for ${CI_ENVIRONMENT_SLUG}"
+
 indexPropertiesFile="./src/main/resources/index.properties"
 eval baseDirectory='$'"index_files_baseDirectory"
 eval ftpUrl='$'"index_files_ftpUrl"
 sed -i -e "s@\(files\.baseDirectory=\).*@\1${baseDirectory}@g" $indexPropertiesFile
 sed -i -e "s@\(files\.ftpUrl=\).*@\1${ftpUrl}@g" $indexPropertiesFile
+
 
 securityPropertiesFile="./src/main/resources/security.properties"
 eval profileUrl='$'"security_auth_profileUrl"
