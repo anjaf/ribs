9b9558457d721de24e4614734590bd1a26423774
Awais Athar <awais@ebi.ac.uk>
2019-10-03 10:07:39 +0100

    Updated jdkCommand to enable debugging

--- tasks/deploy.sh
+++ tasks/deploy.sh
@@ -8,7 +8,7 @@ chmod 700 ~/.ssh/id_rsa
 eval $(ssh-agent -s)
 ssh-add ~/.ssh/id_rsa
 
-eval jdkHome='$'"jdkHome"
+eval jdkCommandLine='$'"jdkCommandLine"
 eval deployDirectory='$'"deployDirectory"
 
 #deploy
@@ -18,5 +18,5 @@ scp -oStrictHostKeyChecking=no ./target/biostudies.war "${user}@${host}:${deploy
 ssh -oStrictHostKeyChecking=no "${user}@${host}" << ENDSSH
 cd $deployDirectory
 pkill -9 -f biostudies
-$jdkHome -Dbiostudies -Dtomcat.hostname=\$(hostname -s) -Xmx12G -jar ./biostudies.war > /dev/null 2>&1 &
+jdkCommandLine -Dtomcat.hostname=\$(hostname -s) -Xmx12G -jar ./biostudies.war > /dev/null 2>&1 &
 ENDSSH
