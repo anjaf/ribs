879ab79893d3fc21c9157a8eff765e6885c61170
Awais Athar <awais@ebi.ac.uk>
2019-11-06 15:33:29 +0000

    Added content-type and length to error page

--- src/main/java/uk/ac/ebi/biostudies/config/CustomErrorReportValve.java
+++ src/main/java/uk/ac/ebi/biostudies/config/CustomErrorReportValve.java
@@ -4,9 +4,12 @@ import org.apache.catalina.connector.Request;
 import org.apache.catalina.connector.Response;
 import org.apache.catalina.valves.ErrorReportValve;
 import org.apache.commons.io.IOUtils;
+import org.apache.http.entity.ContentType;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.Objects;
 
 public class CustomErrorReportValve extends ErrorReportValve {
@@ -22,10 +25,12 @@ public class CustomErrorReportValve extends ErrorReportValve {
             logger.error(throwable);
         }
         try {
-            IOUtils.copy(
-                    Objects.requireNonNull(this.getClass().getClassLoader().getResourceAsStream("static/error.html")),
-                    response.getOutputStream()
-            );
+            String errorPage = new String( Files.readAllBytes( Paths.get(
+                            this.getClass().getClassLoader().getResource("static/error.html").toURI()
+                    )));
+            response.setContentType(String.valueOf(ContentType.TEXT_HTML));
+            response.setContentLength(errorPage.length());
+            response.getOutputStream().print(errorPage);
         } catch (Exception e) {
             e.printStackTrace();
         }
