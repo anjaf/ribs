c0ac5f0f3de9329e93e02209af4465dca380a62e
behrangi <rahmat3616>
2020-07-07 04:23:36 +0100

    Writing test case for file download api with secretkey [#173227799]

--- src/test/java/uk/ac/ebi/biostudies/integration/rest/FileRestApiTest.java
+++ src/test/java/uk/ac/ebi/biostudies/integration/rest/FileRestApiTest.java
@@ -18,7 +18,14 @@ import org.springframework.boot.test.mock.mockito.SpyBean;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.core.io.InputStreamResource;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.ByteArrayHttpMessageConverter;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
 import uk.ac.ebi.biostudies.api.util.Constants;
 import uk.ac.ebi.biostudies.auth.UserSecurityService;
 import uk.ac.ebi.biostudies.config.IndexConfig;
@@ -27,6 +34,10 @@ import uk.ac.ebi.biostudies.service.SearchService;
 import uk.ac.ebi.biostudies.service.ZipDownloadService;
 
 
+import javax.validation.constraints.AssertTrue;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 import static org.junit.Assert.*;
 import static org.mockito.Mockito.doReturn;
@@ -66,9 +77,9 @@ public class FileRestApiTest {
      * https://wwwdev.ebi.ac.uk/biostudies/api/v1/studies/ACCESSION
      */
     public void getStudyFromRestAPI() throws Exception {
-        doReturn(new InputStreamResource(getClass().getClassLoader().getResource(ACCESSION+".json").openStream())).when(searchServiceMock).getStudyAsStream(Mockito.anyString(), Mockito.anyString(), Mockito.anyBoolean());
+        doReturn(new InputStreamResource(getClass().getClassLoader().getResource(ACCESSION + ".json").openStream())).when(searchServiceMock).getStudyAsStream(Mockito.anyString(), Mockito.anyString(), Mockito.anyBoolean());
         String baseUrl = integrationTestProperties.getBaseUrl(randomPort);
-        String result = testRestTemplate.getForObject(baseUrl + "api/v1/studies/"+ACCESSION, String.class);
+        String result = testRestTemplate.getForObject(baseUrl + "api/v1/studies/" + ACCESSION, String.class);
         ObjectMapper mapper = new ObjectMapper();
         JsonNode responseJSON = mapper.readTree(result);
         assertNotNull(responseJSON);
@@ -83,13 +94,13 @@ public class FileRestApiTest {
 
     @Test
     public void getStudyWithSecKeyFromBean() throws Exception {
-        String pathToFile = getClass().getClassLoader().getResource(ACCESSION+".json").getPath().replaceAll("/S-EPMC3372839.json", "");
+        String pathToFile = getClass().getClassLoader().getResource(ACCESSION + ".json").getPath().replaceAll("/S-EPMC3372839.json", "");
         if ((pathToFile.charAt(0) == '\\' || pathToFile.charAt(0) == '/') && pathToFile.charAt(2) == ':')
             pathToFile = pathToFile.substring(1);
         doReturn(pathToFile).when(indexConfigMock).getFileRootDir();
         InputStreamResource myFileStream = searchServiceImpl.getStudyAsStream(ACCESSION, "", true);
         ReadContext jsonPathContext = JsonPath.parse(myFileStream.getInputStream());
-        JSONArray authors =  jsonPathContext.read("$.section.subsections[?(@.type==\"Author\")].attributes[?(@.name==\"Name\")].value");
+        JSONArray authors = jsonPathContext.read("$.section.subsections[?(@.type==\"Author\")].attributes[?(@.name==\"Name\")].value");
         JSONArray organizations = jsonPathContext.read("$.section.subsections[?(@.type==\"Organization\")].attributes[?(@.name==\"Name\")].value");
         assertEquals(0, authors.size());
         assertEquals(0, organizations.size());
@@ -100,9 +111,9 @@ public class FileRestApiTest {
      * https://wwwdev.ebi.ac.uk/biostudies/api/v1/studies/ACCESSION/info
      */
     public void getFileInfoRestApiPublicAccess() throws Exception {
-        doReturn(new InputStreamResource(getClass().getClassLoader().getResource(ACCESSION+".json").openStream())).when(searchServiceMock).getStudyAsStream(Mockito.anyString(), Mockito.anyString(), Mockito.anyBoolean());
+        doReturn(new InputStreamResource(getClass().getClassLoader().getResource(ACCESSION + ".json").openStream())).when(searchServiceMock).getStudyAsStream(Mockito.anyString(), Mockito.anyString(), Mockito.anyBoolean());
         String baseUrl = integrationTestProperties.getBaseUrl(randomPort);
-        String result = testRestTemplate.getForObject(baseUrl + "api/v1/studies/"+ACCESSION+"/info", String.class);
+        String result = testRestTemplate.getForObject(baseUrl + "api/v1/studies/" + ACCESSION + "/info", String.class);
         ObjectMapper mapper = new ObjectMapper();
         JsonNode responseJSON = mapper.readTree(result);
         assertNotNull(responseJSON);
@@ -124,7 +135,7 @@ public class FileRestApiTest {
 
         doReturn(privateLuceneDoc).when(searchServiceMock).getDocumentByAccession(Mockito.anyString(), Mockito.any());
         String baseUrl = integrationTestProperties.getBaseUrl(randomPort);
-        String result = testRestTemplate.getForObject(baseUrl + "api/v1/studies/"+ACCESSION+"/info", String.class);
+        String result = testRestTemplate.getForObject(baseUrl + "api/v1/studies/" + ACCESSION + "/info", String.class);
         JsonNode responseJSON = mapper.readTree(result);
         assertNotNull(responseJSON);
         assertEquals("test12345", responseJSON.findValue("seckey").asText());
@@ -138,7 +149,7 @@ public class FileRestApiTest {
     public void getSimilarStudiesWithoutSecretKeyRestApi() throws Exception {
         ObjectMapper mapper = new ObjectMapper();
         String baseUrl = integrationTestProperties.getBaseUrl(randomPort);
-        String result = testRestTemplate.getForObject(baseUrl + "api/v1/studies/"+ACCESSION+"/similar", String.class);
+        String result = testRestTemplate.getForObject(baseUrl + "api/v1/studies/" + ACCESSION + "/similar", String.class);
         JsonNode responseJSON = mapper.readTree(result);
         assertNotNull(responseJSON);
         assertTrue(responseJSON.get("similarStudies").isArray());
@@ -152,7 +163,7 @@ public class FileRestApiTest {
     public void getSimilarStudiesWithSecretKeyRestApi() throws Exception {
         ObjectMapper mapper = new ObjectMapper();
         String baseUrl = integrationTestProperties.getBaseUrl(randomPort);
-        String result = testRestTemplate.getForObject(baseUrl + "api/v1/studies/"+ACCESSION+"/similar?key=test12345", String.class);
+        String result = testRestTemplate.getForObject(baseUrl + "api/v1/studies/" + ACCESSION + "/similar?key=test12345", String.class);
         JsonNode responseJSON = mapper.readTree(result);
         assertNotNull(responseJSON);
         assertNull(responseJSON.get("similarStudies"));
@@ -165,7 +176,7 @@ public class FileRestApiTest {
     public void getFileListForFileDataTableRestApi() throws Exception {
         ObjectMapper mapper = new ObjectMapper();
         String baseUrl = integrationTestProperties.getBaseUrl(randomPort);
-        String result = testRestTemplate.getForObject(baseUrl + "api/v1/files/"+ACCESSION, String.class);
+        String result = testRestTemplate.getForObject(baseUrl + "api/v1/files/" + ACCESSION, String.class);
         JsonNode responseJSON = mapper.readTree(result);
         assertNotNull(responseJSON);
         assertEquals(1, responseJSON.get("recordsTotal").asInt());
@@ -179,7 +190,7 @@ public class FileRestApiTest {
      */
     public void fileGetDownloadRestApi() throws Exception {
         String baseUrl = integrationTestProperties.getBaseUrl(randomPort);
-        String pathToFile = getClass().getClassLoader().getResource(ACCESSION+".json").getPath().replaceAll("/S-EPMC3372839.json", "");
+        String pathToFile = getClass().getClassLoader().getResource(ACCESSION + ".json").getPath().replaceAll("/S-EPMC3372839.json", "");
         if ((pathToFile.charAt(0) == '\\' || pathToFile.charAt(0) == '/') && pathToFile.charAt(2) == ':')
             pathToFile = pathToFile.substring(1);
         doReturn(pathToFile).when(indexConfigMock).getFileRootDir();
@@ -190,7 +201,7 @@ public class FileRestApiTest {
         privateLuceneDoc.add(new StringField(Constants.Fields.SECRET_KEY, "test12345", Field.Store.YES));
         doReturn(privateLuceneDoc).when(searchServiceMock).getDocumentByAccession(Mockito.anyString(), Mockito.any());
 
-        String result = testRestTemplate.getForObject(baseUrl + "files/"+ACCESSION+"/"+ACCESSION+".json", String.class);
+        String result = testRestTemplate.getForObject(baseUrl + "files/" + ACCESSION + "/" + ACCESSION + ".json", String.class);
         assertNotNull(result);
         ObjectMapper mapper = new ObjectMapper();
         assertEquals(ACCESSION, mapper.readTree(result).findValue("accno").asText());
@@ -203,7 +214,7 @@ public class FileRestApiTest {
      */
     public void fileGetDownloadJsonWithSecretKeyRestApi() throws Exception {
         String baseUrl = integrationTestProperties.getBaseUrl(randomPort);
-        String pathToFile = getClass().getClassLoader().getResource(ACCESSION+".json").getPath().replaceAll("/S-EPMC3372839.json", "");
+        String pathToFile = getClass().getClassLoader().getResource(ACCESSION + ".json").getPath().replaceAll("/S-EPMC3372839.json", "");
         if ((pathToFile.charAt(0) == '\\' || pathToFile.charAt(0) == '/') && pathToFile.charAt(2) == ':')
             pathToFile = pathToFile.substring(1);
         doReturn(pathToFile).when(indexConfigMock).getFileRootDir();
@@ -214,7 +225,7 @@ public class FileRestApiTest {
         privateLuceneDoc.add(new StringField(Constants.Fields.SECRET_KEY, "test12345", Field.Store.YES));
         doReturn(privateLuceneDoc).when(searchServiceMock).getDocumentByAccession(Mockito.anyString(), Mockito.any());
 
-        String result = testRestTemplate.getForObject(baseUrl + "files/"+ACCESSION+"/"+ACCESSION+".json?key=test12345", String.class);
+        String result = testRestTemplate.getForObject(baseUrl + "files/" + ACCESSION + "/" + ACCESSION + ".json?key=test12345", String.class);
         assertNotNull(result);
         assertTrue(result.contains("forbidden"));
     }
@@ -237,8 +248,37 @@ public class FileRestApiTest {
         privateLuceneDoc.add(new StringField(Constants.Fields.SECRET_KEY, "test12345", Field.Store.YES));
         doReturn(privateLuceneDoc).when(searchServiceMock).getDocumentByAccession(Mockito.anyString(), Mockito.any());
 
-        String result = testRestTemplate.getForObject(baseUrl + "files/"+ACCESSION+"/test.properties?key=test12345", String.class);
+        String result = testRestTemplate.getForObject(baseUrl + "files/" + ACCESSION + "/test.properties?key=test12345", String.class);
         assertNotNull(result);
         assertTrue(result.contains("test"));
+    }
+
+    @Test
+    public void zipFileDownloadPostRestApi() throws Exception {
+        String baseUrl = integrationTestProperties.getBaseUrl(randomPort);
+        String pathToFile = getClass().getClassLoader().getResource("updates/files/A.txt").getPath().replaceAll("/updates/files/A.txt", "");
+        if ((pathToFile.charAt(0) == '\\' || pathToFile.charAt(0) == '/') && pathToFile.charAt(2) == ':')
+            pathToFile = pathToFile.substring(1);
+        doReturn(pathToFile).when(indexConfigMock).getFileRootDir();
+        Document privateLuceneDoc = new Document();
+        privateLuceneDoc.add(new StringField(Constants.Fields.ACCESS, "test1", Field.Store.YES));
+        privateLuceneDoc.add(new StringField(Constants.Fields.ACCESSION, ACCESSION, Field.Store.YES));
+        privateLuceneDoc.add(new StringField(Constants.Fields.RELATIVE_PATH, "updates", Field.Store.YES));
+        privateLuceneDoc.add(new StringField(Constants.Fields.SECRET_KEY, "test12345", Field.Store.YES));
+        doReturn(privateLuceneDoc).when(searchServiceMock).getDocumentByAccession(Mockito.anyString(), Mockito.any());
+        MultiValueMap<String, String> map = new LinkedMultiValueMap<>();
+        map.add("type", "zip");
+        map.add("files", "A.txt");
+        map.add("files", "B.txt");
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
+        headers.setAccept(Arrays.asList(MediaType.ALL));
+        HttpEntity<MultiValueMap<String, String>> request = new HttpEntity<>(map, headers);
+
+        testRestTemplate.getRestTemplate().getMessageConverters().add(new ByteArrayHttpMessageConverter());
+        byte[] responseEntity  = testRestTemplate.postForObject(baseUrl + "files/" + ACCESSION + "/zip", request, byte[].class);
+        assertTrue(responseEntity.length>0);
+
     }
 }
