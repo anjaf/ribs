e12f3cc03f46d6009074cdeec46ec0982f371745
behrangi <ehsan.behrangi@gmail.com>
2020-10-21 18:37:50 +0100

    thumbnails not being served for files with special characters []+ in them Submitting [#175357066]

--- src/main/webapp/js/detail/filetable.js
+++ src/main/webapp/js/detail/filetable.js
@@ -185,7 +185,7 @@ var FileTable = (function (_self) {
         if (thumbnailColumn.length) {
             thumbnailColumn[0].render = function (data, type, row) {
             return '<img  height="100" width="100" src="'
-                + window.contextPath + '/thumbnail/' + $('#accession').text() + '/' + row.path + (params.key? '?key='+params.key :'')+'" </img> ';
+                + window.contextPath + '/thumbnail/' + $('#accession').text() + '/' + escape(row.path + (params.key? '?key='+params.key :''))+'" </img> ';
             }
         }
         filesTable = $('#file-list').DataTable({
@@ -522,7 +522,7 @@ var FileTable = (function (_self) {
             if (!$tn.length) return;
             $('#thumbnail-image').html('<i class="fa fa-spinner fa-pulse fa-fw"></i><span class="sr-only">Loading...</span>')
             $('#thumbnail').foundation('open');
-            var img = $("<img />").attr('src', $tn.data('thumbnail')+(key ? '?key='+key :''))
+            var img = $("<img />").attr('src', encodeURIComponent($tn.data('thumbnail')+(key ? '?key='+key :'')).replaceAll('#','%23').replaceAll("+", "%2B").replaceAll("=", "%3D").replaceAll("@", "%40").replaceAll("$", "%24"))
                 .on('load', function() {
                     if (!this.complete || typeof this.naturalWidth == "undefined" || this.naturalWidth == 0) {
                         $('#thumbnail').foundation('close');
