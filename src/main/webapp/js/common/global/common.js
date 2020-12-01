df96661eca9f4f86a82084761839f228c7ce48cd
Awais Athar <awais@ebi.ac.uk>
2020-12-01 12:11:54 +0000

    [Delivers #175921715] Fix title to reflect breadcrumbs

--- src/main/webapp/js/common/global/common.js
+++ src/main/webapp/js/common/global/common.js
@@ -81,7 +81,7 @@ $(function() {
 
 function updateTitleFromBreadCrumbs() {
     //update title
-    var breadcrumbs = $('.breadcrumbs li').map(function  () { return $(this).text().trim(); }).filter(function () { return this!="" && this!='Current:';}).get().reverse();
+    var breadcrumbs = $('.breadcrumbs li').map(function  () { return $(this).text().replaceAll('Current:','').trim(); }).get().reverse();
     document.title = breadcrumbs.length ? breadcrumbs.join(' < ' )+' < EMBL-EBI' : 'BioStudies < EMBL-EBI';
 }
 
