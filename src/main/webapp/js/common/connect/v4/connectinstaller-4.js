a71220a8e2275cf637ea759b297bea7b8e309c68
behrangi <ehsan.behrangi@gmail.com>
2021-01-26 12:36:50 +0000

    Serving Aspera connect plugin from our own server [#174789110]

--- src/main/webapp/js/common/connect/v4/connectinstaller-4.js
+++
@@ -1,9 +0,0 @@
-/*
-  Revision: 3.10.0-812f155
-  Date: 2020-09-23 22:40:27
-
-  http://www.asperasoft.com
-  Copyright IBM Corp. 2008, 2020
-
-  NOTE: This file is expected to be empty. Functionality has been bundled into asperaweb-4.js and asperaweb-4.min.js.
-*/
