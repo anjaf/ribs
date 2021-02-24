3b6afbea7bb392286fd0b698b8c7a6b9d62f7a06
behrangi <ehsan.behrangi@gmail.com>
2021-02-24 01:58:53 +0000

    Enable javascript packaging in JAWR's Environment flags Fixing announcement.properties

--- src/main/java/uk/ac/ebi/biostudies/config/JawrConfig.java
+++ src/main/java/uk/ac/ebi/biostudies/config/JawrConfig.java
@@ -1,22 +1,33 @@
 package uk.ac.ebi.biostudies.config;
 
 import net.jawr.web.servlet.JawrSpringController;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.DependsOn;
+import org.springframework.core.env.Environment;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.support.PropertiesLoaderUtils;
 import org.springframework.web.servlet.HandlerMapping;
 import org.springframework.web.servlet.handler.SimpleUrlHandlerMapping;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Properties;
 
 @Configuration
 public class JawrConfig {
+
+    private static final Logger LOGGER = LogManager.getLogger(JawrConfig.class.getName());
+
     @Autowired
     private JawrSpringController jawrJsController;
     @Autowired
     private JawrSpringController jawrCssController;
+
+    private final static Properties JAWRCONFIG = null;
 
     @Bean
     public HandlerMapping jawrHandlerMapping() {
@@ -27,6 +38,24 @@ public class JawrConfig {
         urlMap.put("**/*.js", this.jawrJsController);
         handlerMapping.setUrlMap(urlMap);
         return handlerMapping;
+    }
+
+    static Properties loadJawrEnvironmentalConfig(Environment environment) {
+        if (JAWRCONFIG != null)
+            return JAWRCONFIG;
+        Resource resource = new ClassPathResource("/jawr.properties");
+        Properties JAWRCONFIG = null;
+        try {
+            JAWRCONFIG = PropertiesLoaderUtils.loadProperties(resource);
+            for (Object key : JAWRCONFIG.keySet()) {
+                if (environment.containsProperty((String) key)) {
+                    JAWRCONFIG.put(key, environment.getProperty((String) key));
+                }
+            }
+        } catch (Exception exception) {
+            LOGGER.error("problem in loading jawr config from environment", exception);
+        }
+        return JAWRCONFIG;
     }
 
 }
--- src/main/java/uk/ac/ebi/biostudies/config/MvcConfig.java
+++ src/main/java/uk/ac/ebi/biostudies/config/MvcConfig.java
@@ -9,16 +9,20 @@ import org.apache.catalina.Context;
 import org.apache.catalina.core.StandardHost;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.server.WebServerFactoryCustomizer;
+import org.springframework.context.EnvironmentAware;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+import org.springframework.core.env.Environment;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.util.AntPathMatcher;
 import org.springframework.web.servlet.config.annotation.*;
+
+import java.util.Properties;
 
 
 @Configuration
@@ -26,8 +30,14 @@ import org.springframework.web.servlet.config.annotation.*;
 @EnableScheduling
 @ComponentScan(basePackages = "uk.ac.ebi.biostudies")
 @PropertySource("classpath:scheduler.properties")
-public class MvcConfig implements WebMvcConfigurer {
+public class MvcConfig implements WebMvcConfigurer, EnvironmentAware {
+    private Environment environment;
 
+
+    @Override
+    public void setEnvironment(final Environment environment) {
+        this.environment = environment;
+    }
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
@@ -120,7 +130,11 @@ public class MvcConfig implements WebMvcConfigurer {
     @Bean
     public JawrSpringController jawrJsController() {
         JawrSpringController jawrJsController = new JawrSpringController();
-        jawrJsController.setConfigLocation("/jawr.properties");
+        Properties config = JawrConfig.loadJawrEnvironmentalConfig(environment);
+        if (config == null)
+            jawrJsController.setConfigLocation("/jawr.properties");
+        else
+            jawrJsController.setConfiguration(config);
         jawrJsController.setType("js");
         return jawrJsController;
     }
@@ -128,7 +142,11 @@ public class MvcConfig implements WebMvcConfigurer {
     @Bean
     public JawrSpringController jawrCssController() {
         JawrSpringController jawrCssController = new JawrSpringController();
-        jawrCssController.setConfigLocation("/jawr.properties");
+        Properties config = JawrConfig.loadJawrEnvironmentalConfig(environment);
+        if (config == null)
+            jawrCssController.setConfigLocation("/jawr.properties");
+        else
+            jawrCssController.setConfiguration(config);
         jawrCssController.setType("css");
         return jawrCssController;
     }
