dbf5d13e35e8b2b2ac9e54527c1e226990c72cd0
behrangi <ehsan.behrangi@gmail.com>
2021-07-20 01:29:42 +0100

    Download zip fails when file counts exceed the limit [#178896523]

--- src/main/java/uk/ac/ebi/biostudies/config/MyTomcatConnectorCustomizer.java
+++ src/main/java/uk/ac/ebi/biostudies/config/MyTomcatConnectorCustomizer.java
@@ -1,17 +1,22 @@
 package uk.ac.ebi.biostudies.config;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.server.WebServerFactoryCustomizer;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Component;
 
 @Configuration
 @Component
 public class MyTomcatConnectorCustomizer implements WebServerFactoryCustomizer<TomcatServletWebServerFactory> {
+    @Autowired
+    private Environment env;
 
     @Override
     public void customize(TomcatServletWebServerFactory factory) {
-        factory.addConnectorCustomizers(connector -> connector.setMaxParameterCount(100000));
+        Integer maxParameterCount = env.getProperty("server.tomcat.max-parameter-count", Integer.class, -1);
+        factory.addConnectorCustomizers(connector -> connector.setMaxParameterCount(maxParameterCount));
     }
 }
