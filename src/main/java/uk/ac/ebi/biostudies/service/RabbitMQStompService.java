9aaa0e44001348df8048ac6e292403ed8d2a08bf
behrangi <ehsan.behrangi@gmail.com>
2021-10-25 01:47:35 +0100

    Configure STOMP on RabbitMQ [#179949712]

---
+++ src/main/java/uk/ac/ebi/biostudies/service/RabbitMQStompService.java
@@ -0,0 +1,74 @@
+package uk.ac.ebi.biostudies.service;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Scope;
+import org.springframework.core.env.Environment;
+import org.springframework.messaging.converter.StringMessageConverter;
+import org.springframework.messaging.simp.stomp.*;
+import org.springframework.stereotype.Service;
+import org.springframework.web.socket.WebSocketHttpHeaders;
+import org.springframework.web.socket.client.WebSocketClient;
+import org.springframework.web.socket.client.standard.StandardWebSocketClient;
+import org.springframework.web.socket.messaging.WebSocketStompClient;
+import uk.ac.ebi.biostudies.config.SecurityConfig;
+
+/**
+ * Ehsan
+ */
+@Service
+@Scope("singleton")
+public class RabbitMQStompService implements InitializingBean, DisposableBean {
+
+    private static Logger logger = LogManager.getLogger(RabbitMQStompService.class);
+    @Autowired
+    SecurityConfig securityConfig;
+    @Autowired
+    private Environment env;
+
+    @Override
+    public void destroy() throws Exception {
+
+    }
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        if (!env.getProperty("spring.rabbitmq.stomp.enable", Boolean.class, false))
+            return;
+        String url = "ws://%s:%s/ws";
+        url = String.format(url, securityConfig.getStompHost(), securityConfig.getStompPort());
+        WebSocketClient client = new StandardWebSocketClient();
+        WebSocketStompClient stompClient = new WebSocketStompClient(client);
+        stompClient.setMessageConverter(new StringMessageConverter());
+        final StompHeaders stompHeaders = new StompHeaders();
+        stompHeaders.add(StompHeaderAccessor.STOMP_LOGIN_HEADER, securityConfig.getStompLoginUser());
+        stompHeaders.add(StompHeaderAccessor.STOMP_PASSCODE_HEADER, securityConfig.getStompPassword());
+        stompHeaders.add(StompHeaderAccessor.STOMP_ACCEPT_VERSION_HEADER, "1.1,1.2");
+        RabbitMQStompSessionHandler sessionHandler = new RabbitMQStompSessionHandler();
+        stompClient.connect(url, new WebSocketHttpHeaders(), stompHeaders, sessionHandler);
+    }
+
+    private class RabbitMQStompSessionHandler extends StompSessionHandlerAdapter {
+        @Override
+        public void afterConnected(StompSession session, StompHeaders connectedHeaders) {
+            String submissionPartialQueue = env.getProperty("partial.submission.rabbitmq.queue", String.class, "/queue/submission-submitted-partials-queue");
+            session.subscribe(submissionPartialQueue, this);
+        }
+
+        @Override
+        public void handleFrame(StompHeaders headers, Object payload) {
+            String msg = (String) payload;
+            logger.info("Received : " + msg);
+        }
+
+        @Override
+        public void handleException(StompSession session, StompCommand command, StompHeaders headers, byte[] payload, Throwable exception) {
+            logger.error("Got an exception", exception);
+        }
+    }
+
+
+}
